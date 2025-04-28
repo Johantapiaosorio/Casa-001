18a92464735f86f1dbdc6c88a7031fd37877b318
Johantapiaosorio <johantapiaosorio@gmail.com>
2025-04-28 04:30:32 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,83 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_3041",
+      "name": "IMG_3041",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 79.5,
+      "initialViewParameters": {
+        "yaw": 0.8792881733924514,
+        "pitch": 0.5698968388555574,
+        "fov": 1.1260492535098268
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0524405352221713,
+          "pitch": 0.06996453574254247,
+          "rotation": 0,
+          "target": "0-img_3041"
+        },
+        {
+          "yaw": 0.07254845927788978,
+          "pitch": -0.038673125665292574,
+          "rotation": 0,
+          "target": "1-img_3042"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_3042",
+      "name": "IMG_3042",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 79.5,
+      "initialViewParameters": {
+        "yaw": -3.0510594278666865,
+        "pitch": 0.04272023110474876,
+        "fov": 1.1260492535098268
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0141649009738654,
+          "pitch": 0.22887448126990684,
+          "rotation": 0,
+          "target": "0-img_3041"
+        },
+        {
+          "yaw": -2.7992014334226933,
+          "pitch": 0.035000638758980074,
+          "rotation": 0,
+          "target": "1-img_3042"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
